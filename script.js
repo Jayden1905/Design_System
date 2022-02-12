98c6da8be018406fef8d3a859e230735248b4b6c
Jayden1905 <yawzyn001@mymail.sim.edu.sg>
2022-02-12 22:13:49 +0630

    add neceessary index pages such as crew page, destination page and technology page

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-const link = document.querySelector(".nav-link");
 const navToggle = document.querySelector(".nav-toggle");
 const navBar = document.querySelector(".nav-bar");
+const marker = document.querySelector("#marker");
+const menuItem = document.querySelectorAll(".nav-link");
+const currentLocation = location.href;
+const menuLength = menuItem.length;
 
 navToggle.addEventListener("click", () => {
   navToggle.classList.toggle("nav-toggle-active");
   navBar.classList.toggle("left-twenty");
 });
 
-document.querySelectorAll(".nav-link").forEach((n) =>
+document.querySelectorAll(".nav-list").forEach((n) =>
   n.addEventListener("click", () => {
     navBar.classList.remove("left-twenty");
+    navToggle.classList.remove("nav-toggle-active");
   })
 );
+
+for (let i = 0; i < menuLength; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].className = "nav-link active-link";
+  }
+}
