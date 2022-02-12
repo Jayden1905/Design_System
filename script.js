ca448cd5b2ecf998598de704de366b53d6213097
Jayden1905 <yawzyn001@mymail.sim.edu.sg>
2022-02-12 16:06:10 +0630

    finish navigation bar which is responsive and with animation but still one animation that I need to work on

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const link = document.querySelector(".nav-link");
+const navToggle = document.querySelector(".nav-toggle");
+const navBar = document.querySelector(".nav-bar");
+
+navToggle.addEventListener("click", () => {
+  navToggle.classList.toggle("nav-toggle-active");
+  navBar.classList.toggle("left-twenty");
+});
 
 document.querySelectorAll(".nav-link").forEach((n) =>
   n.addEventListener("click", () => {
-    link.classList.add("active");
-    console.log("It trigger");
+    navBar.classList.remove("left-twenty");
   })
 );
